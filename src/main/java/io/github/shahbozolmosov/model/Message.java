26d1ad40ffd71850853cf0ff218984eb769b76f0
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-08-09 16:47:28 +0500

    feat: add From to Message model

--- src/main/java/io/github/shahbozolmosov/model/Message.java
+++ src/main/java/io/github/shahbozolmosov/model/Message.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record Message(
         @JsonProperty("message_id")
         long messageId,
+        From from,
         Chat chat,
         String text
 ) {

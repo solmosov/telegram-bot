0e021157fea5a8a8993cdcd9c5588ecfa82ccdaf
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-08-10 10:39:31 +0500

    feat: add new size length validation via http response Content-Length to LimitedBodyHandler

--- src/main/java/io/github/shahbozolmosov/client/LimitedBodyHandler.java
+++ src/main/java/io/github/shahbozolmosov/client/LimitedBodyHandler.java
@@ -1,5 +1,7 @@
 package io.github.shahbozolmosov.client;
 
+import io.github.shahbozolmosov.exception.TelegramClientException;
+
 import java.net.http.HttpResponse;
 
 public class LimitedBodyHandler implements HttpResponse.BodyHandler<byte[]> {
@@ -12,6 +14,16 @@ public class LimitedBodyHandler implements HttpResponse.BodyHandler<byte[]> {
 
     @Override
     public HttpResponse.BodySubscriber<byte[]> apply(HttpResponse.ResponseInfo responseInfo) {
+        long contentLength = responseInfo.headers()
+                .firstValueAsLong("Content-Length")
+                .orElse(-1);
+
+        if (contentLength > maxBytes) {
+            throw new TelegramClientException(
+                    "Telegram API response is too large"
+            );
+        }
+
         return new LimitedBodySubscriber(maxBytes);
     }
 }

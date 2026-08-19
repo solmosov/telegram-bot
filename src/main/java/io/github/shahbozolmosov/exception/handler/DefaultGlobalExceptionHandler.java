b179330b549affd64bee144e6fe1420cac6e5817
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-08-19 22:01:03 +0500

    feat: add new methods to DefaultGlobalExceptionHandler

--- src/main/java/io/github/shahbozolmosov/exception/handler/DefaultGlobalExceptionHandler.java
+++ src/main/java/io/github/shahbozolmosov/exception/handler/DefaultGlobalExceptionHandler.java
@@ -1,15 +1,67 @@
 package io.github.shahbozolmosov.exception.handler;
 
 import io.github.shahbozolmosov.exception.GlobalExceptionHandler;
+import io.github.shahbozolmosov.exception.api.TelegramApiException;
+import io.github.shahbozolmosov.exception.authorization.AccessDeniedException;
+import io.github.shahbozolmosov.exception.client.TelegramClientException;
 import io.github.shahbozolmosov.model.Update;
 
+// TODO change serr log message replace with Slf4j dependency
 public class DefaultGlobalExceptionHandler implements GlobalExceptionHandler {
     @Override
     public void handle(Exception exception, Update update) {
-        System.err.println("[Telegram Bot] Handler error for update "
-                + update.updateId() + ": " + exception.getMessage());
+        if (exception instanceof TelegramApiException ex) {
+            handleApiException(ex);
+            return;
+        }
 
-        // TODO: replace with logger dependency. Slf4j
+        if (exception instanceof TelegramClientException ex) {
+            handleClientException(ex, update);
+            return;
+        }
+
+        if (exception instanceof AccessDeniedException ex) {
+            handleAccessDeniedException(ex, update);
+            return;
+        }
+
+        handleUnexpectedException(exception, update);
+    }
+
+    private void handleApiException(TelegramApiException ex) {
+        System.err.println(
+                "[Telegram Bot] Handler error for update "
+                        + " | errorCode=" + ex.getErrorCode()
+                        + " | message=" + ex.getMessage()
+        );
+    }
+
+
+    private void handleClientException(TelegramClientException ex, Update update) {
+        System.err.println(
+                "[Telegram Bot] Telegram client error"
+                        + " | updateId=" + update.updateId()
+                        + " | message=" + update.message()
+        );
+    }
+
+
+    private void handleAccessDeniedException(AccessDeniedException ex, Update update) {
+        System.err.println(
+                "[Telegram Bot] Access denied"
+                        + " | updatedId=" + update.updateId()
+                        + " | message=" + ex.getMessage()
+        );
+    }
+
+    private void handleUnexpectedException(Exception exception, Update update) {
+        System.err.println(
+                "[Telegram Bot] Unexpected error"
+                        + " | updateId=" + update.updateId()
+                        + " | message=" + exception.getMessage()
+        );
+
         exception.printStackTrace();
     }
+
 }

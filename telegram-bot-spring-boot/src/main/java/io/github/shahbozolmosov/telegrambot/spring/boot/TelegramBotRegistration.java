d0d470ce8b0e58621203ecf244ee4bafd1a92d8f
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-08-22 19:43:08 +0500

    refactored: optimized TelegramBotRegistration

--- telegram-bot-spring-boot/src/main/java/io/github/shahbozolmosov/telegrambot/spring/boot/TelegramBotRegistration.java
+++ telegram-bot-spring-boot/src/main/java/io/github/shahbozolmosov/telegrambot/spring/boot/TelegramBotRegistration.java
@@ -7,7 +7,37 @@ public record TelegramBotRegistration(
         String token,
         TelegramBotConfig config
 ) {
-    public TelegramBotRegistration(String botName, String token){
+    public TelegramBotRegistration(String botName, String token) {
         this(botName, token, null);
     }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String botName;
+        private String token;
+        private TelegramBotConfig config;
+
+        private Builder() {
+        }
+
+        public void setBotName(String name) {
+            this.botName = name;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+
+        public void setConfig(TelegramBotConfig config) {
+            this.config = config;
+        }
+
+        public TelegramBotRegistration build() {
+            return new TelegramBotRegistration(botName, token, config);
+        }
+    }
 }
+

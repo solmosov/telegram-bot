76a5cee64b70ba4d1b7fa7f7669540d2058e3d29
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-08-24 12:32:52 +0500

    refactored: SendMessageRequest and add new static methods

--- telegram-bot-core/src/main/java/io/github/shahbozolmosov/telegrambot/request/message/SendMessageRequest.java
+++ telegram-bot-core/src/main/java/io/github/shahbozolmosov/telegrambot/request/message/SendMessageRequest.java
@@ -3,6 +3,7 @@ package io.github.shahbozolmosov.telegrambot.request.message;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.shahbozolmosov.telegrambot.keyboard.ReplyMarkup;
+import io.github.shahbozolmosov.telegrambot.keyboard.reply.ReplyKeyboardMarkup;
 import io.github.shahbozolmosov.telegrambot.model.ParseMode;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -16,14 +17,105 @@ public record SendMessageRequest(
         ParseMode parseMode,
 
         @JsonProperty("reply_markup")
-        ReplyMarkup replyMarkup
+        ReplyMarkup replyMarkup,
+
+        @JsonProperty("disable_web_page_preview")
+        Boolean disableWebPagePreview
 ) {
     public SendMessageRequest(
             String chatId,
             String text,
-            ReplyMarkup replyMarkup
+            ReplyMarkup replyMarkup,
+            Boolean disableWebPagePreview
     ) {
-        this(chatId, text, null, replyMarkup);
+        this(chatId, text, null, replyMarkup, disableWebPagePreview);
+    }
+
+    /*------------------------------- TEXT --------------------------------------------*/
+    public static SendMessageRequest text(String chatId, String text) {
+        return text(chatId, text, null);
+    }
+
+    public static SendMessageRequest text(String chatId, String text, ReplyMarkup replyMarkup) {
+        return text(chatId, text, replyMarkup, null);
+    }
+
+    public static SendMessageRequest text(String chatId, String text, ReplyMarkup replyMarkup, Boolean disableWebPagePreview) {
+        return new SendMessageRequest(
+                chatId,
+                text,
+                null,
+                replyMarkup,
+                disableWebPagePreview
+        );
+    }
+
+    /*------------------------------- Markdown --------------------------------------------*/
+    public static SendMessageRequest markdown(String chatId, String text) {
+        return markdown(chatId, text, null, null);
+    }
+
+    public static SendMessageRequest markdown(String chatId, String text, ReplyMarkup replyMarkup){
+        return markdown(chatId, text, replyMarkup, null);
+    }
+
+    public static SendMessageRequest markdown(String chatId, String text, ReplyMarkup replyMarkup, Boolean disableWebPagePreview) {
+        return new SendMessageRequest(
+                chatId,
+                text,
+                ParseMode.MARKDOWN,
+                replyMarkup,
+                disableWebPagePreview
+        );
+    }
+
+    /*------------------------------- Markdown V2 --------------------------------------------*/
+    public static SendMessageRequest markdownV2(String chatId, String text) {
+        return markdownV2(chatId, text, null, null);
+    }
+
+    public static SendMessageRequest markdownV2(String chatId, String text, ReplyMarkup replyMarkup){
+        return markdownV2(chatId, text, replyMarkup, null);
+    }
+
+    public static SendMessageRequest markdownV2(String chatId, String text, ReplyMarkup replyMarkup, Boolean disableWebPagePreview) {
+        return new SendMessageRequest(
+                chatId,
+                text,
+                ParseMode.MARKDOWN_V2,
+                replyMarkup,
+                disableWebPagePreview
+        );
+    }
+
+    /*------------------------------- Html --------------------------------------------*/
+    public static SendMessageRequest html(String chatId, String text) {
+        return html(chatId, text, null, null);
+    }
+
+    public static SendMessageRequest html(String chatId, String text, ReplyMarkup replyMarkup){
+        return html(chatId, text, replyMarkup, null);
+    }
+
+    public static SendMessageRequest html(String chatId, String text, ReplyMarkup replyMarkup, Boolean disableWebPagePreview) {
+        return new SendMessageRequest(
+                chatId,
+                text,
+                ParseMode.HTML,
+                replyMarkup,
+                disableWebPagePreview
+        );
+    }
+
+    /*------------------------------- Remove Keyboard --------------------------------------------*/
+    public static SendMessageRequest removeReplyKeyboard(String chatId, String text){
+        return new SendMessageRequest(
+                chatId,
+                text,
+                null,
+                ReplyKeyboardMarkup.remove(),
+                null
+        );
     }
 
 }

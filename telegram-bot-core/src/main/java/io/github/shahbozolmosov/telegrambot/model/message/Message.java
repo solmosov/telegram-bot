b0f1870ddfa4fd54aef5c86d0851017a5b7e4b4c
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-08-27 08:20:51 +0500

    feat: add message new model

---
+++ telegram-bot-core/src/main/java/io/github/shahbozolmosov/telegrambot/model/message/Message.java
@@ -0,0 +1,39 @@
+package io.github.shahbozolmosov.telegrambot.model.message;
+
+import io.github.shahbozolmosov.telegrambot.model.Chat;
+import io.github.shahbozolmosov.telegrambot.model.From;
+
+public class Message {
+    private final long messageId;
+    private final From from;
+    private final Chat chat;
+    private final long date;
+
+    protected Message(
+            long messageId,
+            From from,
+            Chat chat,
+            long date
+    ) {
+        this.messageId = messageId;
+        this.from = from;
+        this.chat = chat;
+        this.date = date;
+    }
+
+    public long messageId() {
+        return messageId;
+    }
+
+    public From from() {
+        return from;
+    }
+
+    public Chat chat() {
+        return chat;
+    }
+
+    public long date() {
+        return date;
+    }
+}

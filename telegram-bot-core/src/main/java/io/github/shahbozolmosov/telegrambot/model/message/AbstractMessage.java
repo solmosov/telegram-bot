263d523eb539c8c495010333e01bb23bd61adaae
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-08-27 08:41:05 +0500

    refactored: renamed Message.java to AbstractMessage.java

--- telegram-bot-core/src/main/java/io/github/shahbozolmosov/telegrambot/model/message/AbstractMessage.java
+++ telegram-bot-core/src/main/java/io/github/shahbozolmosov/telegrambot/model/message/AbstractMessage.java
@@ -3,13 +3,13 @@ package io.github.shahbozolmosov.telegrambot.model.message;
 import io.github.shahbozolmosov.telegrambot.model.Chat;
 import io.github.shahbozolmosov.telegrambot.model.From;
 
-public class Message {
+public abstract class AbstractMessage {
     private final long messageId;
     private final From from;
     private final Chat chat;
     private final long date;
 
-    protected Message(
+    protected AbstractMessage(
             long messageId,
             From from,
             Chat chat,

171f9333aa3d42aac57cb40d0222914e3d09dcc8
Shahboz Olmosov <83070485+solmosov@users.noreply.github.com>
2026-08-28 19:20:17 +0500

    feat: add all other sections test to BotContextTest

--- telegram-bot-core/src/test/java/io/github/solmosov/telegrambot/context/BotContextTest.java
+++ telegram-bot-core/src/test/java/io/github/solmosov/telegrambot/context/BotContextTest.java
@@ -1,8 +1,7 @@
 package io.github.solmosov.telegrambot.context;
 
 import io.github.solmosov.telegrambot.client.TelegramClient;
-import io.github.solmosov.telegrambot.context.builder.ChatActionBuilder;
-import io.github.solmosov.telegrambot.context.builder.MessageBuilder;
+import io.github.solmosov.telegrambot.context.builder.*;
 import io.github.solmosov.telegrambot.model.CallbackQuery;
 import io.github.solmosov.telegrambot.model.Chat;
 import io.github.solmosov.telegrambot.model.Message;
@@ -210,46 +209,473 @@ class BotContextTest {
 
     @Nested
     class EditMessageTests {
-        // editMessage()
-        // editMessageCaption()
+        @Test
+        void shouldCreateEditMessageTextBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(
+                            100L,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null
+                    )
+            );
+
+            Update update = new Update(
+                    123L,
+                    message,
+                    null
+            );
+
+            BotContext context = new BotContext(client, update);
+
+            // when
+            EditMessageTextBuilder result =
+                    context.editMessage(456L, "Updated text");
+
+            // then
+            assertNotNull(result);
+        }
+
+        @Test
+        void shouldCreateEditMessageCaptionBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(
+                            100L,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null
+                    )
+            );
+
+            Update update = new Update(
+                    123L,
+                    message,
+                    null
+            );
+
+            BotContext context = new BotContext(client, update);
+
+            // when
+            EditMessageCaptionBuilder result =
+                    context.editMessageCaption(456L, "Updated caption");
+
+            // then
+            assertNotNull(result);
+        }
     }
 
     @Nested
     class DeleteMessageTests {
-        // deleteMessage()
+        @Test
+        void shouldCreateDeleteMessageBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(
+                            100L,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null
+                    )
+            );
+
+            Update update = new Update(
+                    123L,
+                    message,
+                    null
+            );
+
+            BotContext context = new BotContext(client, update);
+
+            // when
+            DeleteMessageBuilder result =
+                    context.deleteMessage(456L);
+
+            // then
+            assertNotNull(result);
+        }
     }
 
     @Nested
     class PhotoTests {
-        // photo()
-        // photo(byte[], ...)
+
+        @Test
+        void shouldCreatePhotoBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            // when
+            PhotoBuilder result = context.photo("https://example.com/photo.jpg");
+
+            // then
+            assertNotNull(result);
+        }
+
+        @Test
+        void shouldCreatePhotoUploadBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            byte[] file = "image".getBytes();
+
+            // when
+            PhotoUploadBuilder result =
+                    context.photo(file, "photo.jpg", "image/jpeg");
+
+            // then
+            assertNotNull(result);
+        }
     }
 
     @Nested
     class VideoTests {
-        // video()
-        // video(byte[], ...)
+
+        @Test
+        void shouldCreateVideoBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            // when
+            VideoBuilder result =
+                    context.video("https://example.com/video.mp4");
+
+            // then
+            assertNotNull(result);
+        }
+
+        @Test
+        void shouldCreateVideoUploadBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            byte[] file = "video".getBytes();
+
+            // when
+            VideoUploadBuilder result =
+                    context.video(file, "video.mp4", "video/mp4");
+
+            // then
+            assertNotNull(result);
+        }
     }
 
     @Nested
     class DocumentTests {
-        // document()
-        // document(byte[], ...)
+
+        @Test
+        void shouldCreateDocumentBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            // when
+            DocumentBuilder result =
+                    context.document("https://example.com/file.pdf");
+
+            // then
+            assertNotNull(result);
+        }
+
+        @Test
+        void shouldCreateDocumentUploadBuilder() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            byte[] file = "document".getBytes();
+
+            // when
+            DocumentUploadBuilder result =
+                    context.document(file, "document.pdf", "application/pdf");
+
+            // then
+            assertNotNull(result);
+        }
     }
 
     @Nested
     class ReplyKeyboardTests {
-        // replyKeyboard()
+
+        @Test
+        void shouldReturnReplyKeyboardContext() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            // when
+            ReplyKeyboardContext result = context.replyKeyboard();
+
+            // then
+            assertNotNull(result);
+        }
+
+        @Test
+        void shouldReturnSameReplyKeyboardContextInstance() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            // when
+            ReplyKeyboardContext first = context.replyKeyboard();
+            ReplyKeyboardContext second = context.replyKeyboard();
+
+            // then
+            assertSame(first, second);
+        }
+    }
+
+    @Nested
+    class PhotoContextTests {
+
+        @Test
+        void shouldReturnPhotoContextWhenMessageExists() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            // when
+            PhotoContext result = context.photo();
+
+            // then
+            assertNotNull(result);
+        }
+
+        @Test
+        void shouldReturnNullPhotoContextForCallbackQuery() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+
+            CallbackQuery callbackQuery = new CallbackQuery(
+                    "callback-id",
+                    null,
+                    message,
+                    "chat-instance",
+                    "data"
+            );
+
+            Update update = new Update(
+                    123L,
+                    null,
+                    callbackQuery
+            );
+
+            BotContext context = new BotContext(client, update);
+
+            // when
+            PhotoContext result = context.photo();
+
+            // then
+            assertNull(result);
+        }
     }
 
     @Nested
     class CallbackQueryTests {
-        // callbackQuery()
+
+        @Test
+        void shouldReturnCallbackQueryContextWhenCallbackQueryExists() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+
+            CallbackQuery callbackQuery = new CallbackQuery(
+                    "callback-id",
+                    null,
+                    message,
+                    "chat-instance",
+                    "data"
+            );
+
+            Update update = new Update(
+                    123L,
+                    null,
+                    callbackQuery
+            );
+
+            BotContext context = new BotContext(client, update);
+
+            // when
+            CallbackQueryContext result = context.callbackQuery();
+
+            // then
+            assertNotNull(result);
+        }
+
+        @Test
+        void shouldReturnNullWhenCallbackQueryDoesNotExist() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(
+                    123L,
+                    message,
+                    null
+            );
+
+            BotContext context = new BotContext(client, update);
+
+            // when
+            CallbackQueryContext result = context.callbackQuery();
+
+            // then
+            assertNull(result);
+        }
     }
 
     @Nested
     class DeepLinkTests {
-        // setDeepLinkParam()
-        // deepLinkParam()
+
+        @Test
+        void shouldReturnNullDeepLinkParamByDefault() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            // when
+            String result = context.deepLinkParam();
+
+            // then
+            assertNull(result);
+        }
+
+        @Test
+        void shouldSetAndReturnDeepLinkParam() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            // when
+            context.setDeepLinkParam("start_123");
+
+            // then
+            assertEquals("start_123", context.deepLinkParam());
+        }
+
+        @Test
+        void shouldReplaceExistingDeepLinkParam() {
+            // given
+            TelegramClient client = mock(TelegramClient.class);
+
+            Message message = mock(Message.class);
+            when(message.chat()).thenReturn(
+                    new Chat(100L, null, null, null, null, null)
+            );
+
+            Update update = new Update(123L, message, null);
+            BotContext context = new BotContext(client, update);
+
+            context.setDeepLinkParam("first");
+
+            // when
+            context.setDeepLinkParam("second");
+
+            // then
+            assertEquals("second", context.deepLinkParam());
+        }
     }
 }

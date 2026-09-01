f3ab3863d8e17f59aa7cadae31d1d25c0855e45e
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-09-01 13:17:27 +0500

    feat: add telegram bot dependency and done for example/spring-boot-webhook

---
+++ examples/spring-boot-webhook/src/main/java/com/example/springbootwebhook/config/TelegramBotConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.springbootwebhook.config;
+
+import io.github.solmosov.telegrambot.spring.boot.TelegramBotRegistration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfiguration {
+
+    @Bean
+    public TelegramBotRegistration myBotRegistration(){
+        return TelegramBotRegistration.builder()
+                .botName("myBot")
+                .token(System.getenv("MY_BOT_TOKEN"))
+                .config(config -> config
+                        .webhookPort(8080)
+                        .webhookPath("/webhook/telegram") // path
+                        .webhookPathSecret(System.getenv("MY_BOT_WEBHOOK_PATH_SECRET")) // your secret for path protection
+                        .webhookSecret(System.getenv("MY_BOT_WEBHOOK_SECRET")) // your secret for header
+                        .webhookUrl(System.getenv("MY_BOT_WEBHOOK_URL")) // https://your-domain.com
+                )
+                .build();
+    }
+}

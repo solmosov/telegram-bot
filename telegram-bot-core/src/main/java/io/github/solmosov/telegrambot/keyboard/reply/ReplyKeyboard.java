d5c5f057da7a61495424e6c8429e8b99657fc2de
Shahboz Olmosov <83070485+shahbozolmosov@users.noreply.github.com>
2026-09-03 11:07:56 +0500

    refactor: remove builder and change methods of ReplyKeyboard

--- telegram-bot-core/src/main/java/io/github/solmosov/telegrambot/keyboard/reply/ReplyKeyboard.java
+++ telegram-bot-core/src/main/java/io/github/solmosov/telegrambot/keyboard/reply/ReplyKeyboard.java
@@ -9,10 +9,16 @@ public final class ReplyKeyboard {
     private ReplyKeyboard() {
     }
 
+    // ---------------- Remove Keyboard ---------------
     public static ReplyKeyboardMarkup removeKeyboard() {
-        return ReplyKeyboardMarkup.remove();
+        return removeKeyboard(null);
     }
 
+    public static ReplyKeyboardMarkup removeKeyboard(Boolean selective) {
+        return ReplyKeyboardMarkup.remove(selective);
+    }
+
+    // ---------------- Buttons ---------------
     public static ReplyKeyboardButton button(String text) {
         return new ReplyKeyboardButton(text);
     }
@@ -22,10 +28,10 @@ public final class ReplyKeyboard {
     }
 
     public static ReplyKeyboardButton buttonContact(String text) {
-        return  ReplyKeyboardButton.contact(text);
+        return ReplyKeyboardButton.contact(text);
     }
 
-    public static ReplyKeyboardButton buttonRequestUsers(String text, int requestId, Consumer<RequestUsers.Builder> consumer){
+    public static ReplyKeyboardButton buttonRequestUsers(String text, int requestId, Consumer<RequestUsers.Builder> consumer) {
         RequestUsers.Builder requestUsersBuilder = RequestUsers.builder(requestId);
 
         consumer.accept(requestUsersBuilder);
@@ -33,12 +39,14 @@ public final class ReplyKeyboard {
         return ReplyKeyboardButton.requestUsers(text, requestUsersBuilder.build());
     }
 
+    // ---------------- Row ---------------
     public static ReplyKeyboardRow row(
             ReplyKeyboardButton... buttons
     ) {
         return new ReplyKeyboardRow(List.of(buttons));
     }
 
+    // ---------------- Of ---------------
     public static ReplyKeyboardMarkup of(
             ReplyKeyboardElement... elements
     ) {
@@ -52,52 +60,6 @@ public final class ReplyKeyboard {
             }
         }
 
-        return new ReplyKeyboardMarkup(rows);
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static final class Builder {
-
-        private boolean resizeKeyboard;
-        private boolean oneTimeKeyboard;
-        private boolean removeKeyboard;
-
-        private final List<List<ReplyKeyboardButton>> rows = new ArrayList<>();
-
-        public Builder resizeKeyboard(boolean value) {
-            this.resizeKeyboard = value;
-            return this;
-        }
-
-        public Builder oneTimeKeyboard(boolean value) {
-            this.oneTimeKeyboard = value;
-            return this;
-        }
-
-        public Builder removeKeyboard(boolean value) {
-            this.removeKeyboard = value;
-            return this;
-        }
-
-        public Builder of(
-                ReplyKeyboardElement... elements
-        ) {
-            for (ReplyKeyboardElement element : elements) {
-                if (element instanceof ReplyKeyboardButton button) {
-                    rows.add(List.of(button));
-                } else if (element instanceof ReplyKeyboardRow(List<ReplyKeyboardButton> buttons)) {
-                    rows.add(buttons);
-                }
-            }
-
-            return this;
-        }
-
-        public ReplyKeyboardMarkup build() {
-            return new ReplyKeyboardMarkup(rows, resizeKeyboard, oneTimeKeyboard, removeKeyboard);
-        }
+        return ReplyKeyboardMarkup.initial(rows);
     }
 }
